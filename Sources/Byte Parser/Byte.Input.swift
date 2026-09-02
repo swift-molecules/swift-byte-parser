abcc0145bbd919151ad9a4636e2c2dd3b9462abd
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-02 21:51:04 +0200

    Imports used only internally are internal

--- Sources/Byte Parser/Byte.Input.swift
+++ Sources/Byte Parser/Byte.Input.swift
@@ -1,8 +1,8 @@
 import Byte
-public import Checkpoint
+import Checkpoint
 public import Cursor
-public import Iterator
-public import Iterator_Protocol
+import Iterator
+import Iterator_Protocol
 
 extension Byte {
 
