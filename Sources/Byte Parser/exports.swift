7215c37302e5d6278fc37eec43d2eb82a76b6dfa
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 11:59:58 +0200

    Tombstone: Byte.Input dissolved into the standard-library slice cursor

--- Sources/Byte Parser/exports.swift
+++
@@ -1,5 +0,0 @@
-@_exported public import Byte
-@_exported public import Cursor
-@_exported public import Iterator_Parser
-@_exported public import Either
-@_exported public import Parser
--- Tests/Support/exports.swift
+++
@@ -1,1 +0,0 @@
-@_exported public import Byte_Parser
